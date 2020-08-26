436525dac73a66ef267e067d062d4a07929479a1
yash kohli <yashkohli786@gmail.com>
2020-08-27 01:25:57 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          {/* if we use <a> tag it will refresh and goes to another pager and we dont want that */}
+          {/* these links convert to clever a tags to apply css we write a tag */}
+          <Link to="/store">Store</Link>
+        </div>
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
