237a2924cbba78b9681923cbc6b0f599b5f0f462
yash kohli <yashkohli786@gmail.com>
2020-08-27 23:39:05 +0530

    home page completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
+import Search from "./Search";
 function Home() {
   return (
     <div className="home">
@@ -25,6 +26,9 @@ function Home() {
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           alt=""
         />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/Search.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Search.css";
+import MicIcon from "@material-ui/icons/Mic";
+import SearchIcon from "@material-ui/icons/Search";
+import { Button } from "@material-ui/core";
+function Search() {
+  const [input, setInput] = useState("");
+  const search = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form className="search">
+      <div className="search__input">
+        <SearchIcon className="search__inputIcon" />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <MicIcon />
+      </div>
+      <div className="search__buttons">
+        {/* variant  is Material Ui prop for buttons */}
+        <Button type="submit" onClick={search} variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
+      </div>
+    </form>
+  );
+}
+
+export default Search;
